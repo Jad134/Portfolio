800725f5dd900779f941d503c04a9725cbd0bd42
Jad El Nader <Jadelnader99@gmail.com>
2024-01-23 10:14:44 +0100

    contact-section

--- src/app/main-content/main-content/main-content.component.ts
+++ src/app/main-content/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { LandingPageComponent } from '../landing-page/landing-page.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { SkillsComponent } from '../skills/skills.component';
 import { PortfolioPageComponent } from '../portfolio-page/portfolio-page.component';
+import { ContactPageComponent } from '../contact-page/contact-page.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent, PortfolioPageComponent],
+  imports: [LandingPageComponent, AboutMeComponent, SkillsComponent, PortfolioPageComponent, ContactPageComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
