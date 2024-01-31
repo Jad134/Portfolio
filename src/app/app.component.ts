9053b96101994e8e4605f84c6aafb07e22e5407e
Jad El Nader <Jadelnader99@gmail.com>
2024-01-31 14:56:35 +0100

    add slide div in until projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
@@ -12,6 +12,55 @@ import { FooterComponent } from './shared/components/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portfolio';
+  //Ab hier kann alles gelöscht werden falls es nicht klappt 
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  
+
+  private directionMap: { [key: string]: string } = {
+    'left': 'slide-in-left',
+    'right': 'slide-in-right'
+  };
+
+  private slideDirection: string = '';
+
+  ngAfterViewInit(): void {
+    this.addIntersectionObserver('.slide-in');
+  }
+
+  private addIntersectionObserver(className: string): void {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5
+    };
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const direction = entry.target.getAttribute('data-direction');
+          if (direction !== null && this.directionMap.hasOwnProperty(direction)) {
+            this.slideDirection = this.directionMap[direction];
+          } else {
+            this.slideDirection = ''; // Fallback auf leeren String, wenn Richtung nicht definiert ist
+          }
+          this.animateSlideIn(entry.target as HTMLElement);
+        } else {
+          this.renderer.removeClass(entry.target as HTMLElement, 'active');
+        }
+      });
+    }, options);
+
+    const elements = this.el.nativeElement.querySelectorAll(className);
+    elements.forEach((element: Element) => {
+      observer.observe(element);
+    });
+  }
+
+  private animateSlideIn(element: HTMLElement): void {
+    this.renderer.addClass(element, 'active');
+    this.renderer.addClass(element, this.slideDirection);
+  }
 }
