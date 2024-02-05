5b728704165b394b59ed4662f2bf32e0c48e9dd9
Jad El Nader <Jadelnader99@gmail.com>
2024-02-05 15:25:00 +0100

    add burger dropdown menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss', './responsive-header.scss']
 })
 export class HeaderComponent {
+  isDropdownOpen: boolean = false;
+  iconState: number = 0;
 
+  
+  getIconSrc(): string {
+    switch (this.iconState) {
+      case 1:
+        return 'assets/img/icons/x.png';
+      case 2:
+        return 'assets/img/icons/burger_menu_transition1.png';
+      case 3:
+        return 'assets/img/icons/burger_menu_transition2.png';
+      default:
+        return 'assets/img/icons/burger_menu.png';
+    }
+  }
+
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+    this.preventScrolling()
+
+    setTimeout(() => {
+      this.iconState = this.isDropdownOpen ? 2 : 3;
+    }, 100);
+
+    setTimeout(() => {
+      this.iconState = this.isDropdownOpen ? 3 : 2;
+    }, 200);
+
+    setTimeout(() => {
+      this.iconState = this.isDropdownOpen ? 1 : 0;
+    }, 300);
+  }
+
+
+  preventScrolling() {
+    if (this.isDropdownOpen) {
+      document.documentElement.style.overflow = 'hidden';
+    } else {
+      document.documentElement.style.overflow = 'auto';
+    }
+  }
 }
+
