83baa99d90dee66da0f3d4ce7bdff56e935eb64a
Jad El Nader <Jadelnader99@gmail.com>
2024-05-24 18:40:58 +0200

    first feedback changes

--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss', './responsive-landingpage.scss']
 })
 export class LandingPageComponent {
-
+  appcomponent = inject(AppComponent)
 }
